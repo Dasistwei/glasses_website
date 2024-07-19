4605ccc9bb87ace274f8dad516ad16bec253b2ae
wei <wei02919@gmail.com>
2024-07-19 22:48:12 +0800

    add base

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/vite/',
+  base: '/glasses_website/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
